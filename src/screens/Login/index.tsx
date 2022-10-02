ae43edbf0fd81ac78068099a0d388ad9781cdd36
lucasmoraessouza <lucas.msouza@hotmail.com>
2022-10-01 22:43:36 -0300

    [FEAT] Create animate in logo into login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -2,15 +2,18 @@ import { View, TextInput, Image } from "react-native";
 import { styles } from "./styles";
 import { Button } from "../../components/Button";
 import { theme } from "../../global/theme";
+import * as Animatable from "react-native-animatable";
 
 export function Login() {
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <Image
-          source={require("../../assets/images/icon.png")}
-          style={styles.logo}
-        />
+        <Animatable.View animation="flipInY">
+          <Image
+            source={require("../../assets/images/icon.png")}
+            style={styles.logo}
+          />
+        </Animatable.View>
         <View style={styles.form}>
           <TextInput
             style={styles.input}
